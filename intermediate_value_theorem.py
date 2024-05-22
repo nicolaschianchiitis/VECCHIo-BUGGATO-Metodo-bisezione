a9672441af1ddc631ac74240a2f2115ebc5ef6e8
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-05-22 12:54:11 +0200

    Iniziato programma
    Iniziato programma

---
+++ intermediate_value_theorem.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def isValid(f, a, b):
+    if  np.sign(f(a)) != np.sign(f(b)):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from bisection import *
+from intermediate_value_theorem import *
+
+
